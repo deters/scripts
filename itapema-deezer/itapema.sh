92170052aeeec9527411b700ca250e28c9aeec90
Lucas Augusto Deters <lucasdeters@gmail.com>
2017-08-26 23:09:34 -0300

    Created script.

---
+++ itapema-deezer/itapema.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+node itapema_musics.js
+node deezer_musics.js
+node deezer_isrc.js
+node select.js
--- itapema-deezer/itapema_musics.js
+++ itapema-deezer/itapema_musics.js
@@ -8,6 +8,11 @@ Tasks:
 var sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./itapema_musics.sqlite');
 
+semaphore = require('semaphore')
+
+let REQUEST_PER_SEC = 1;
+var sem1 = semaphore(REQUEST_PER_SEC);
+
 var request = require('request-promise');
 var cheerio = require('cheerio');         // Web parser. Used to collect Itapema's information.
 var hash = require('object-hash');
@@ -31,44 +36,57 @@ function itapema_list_dates() {
 }
 
 function itapema_list_musics_from_date(date) {
+
+
 	return new Promise( (resolve, reject) => {
-		let music_list = []
 
-		let year = date.substr(4,4);
-		let month = date.substr(2,2);
-		let day = date.substr(0,2);
+		sem1.take(()=>{
+			setTimeout(sem1.leave, 1 * 1000);
 
-		let url_date = day+month+year;
 
-		console.log('requesting '+url_date);
-		let url = `http://www.clicrbs.com.br/especial/sc/itapemafmsc/65,434,15,2,5,2,${url_date},PlayList.html`;
-		request(url).then( html => {
-			var $ = cheerio.load(html);
-			$('table.grade tbody').each( (index, value) => {
-				let tbody = $(value);
-				$('tr', tbody).each ( (index, value) => {
-					let tr = $(value);
-					var music = {}
-					music.origin = 'Itapema'
-					music.year = year;
-					music.month = month;
-					music.day = day;
-					music.time = tr.children().first().text();
-					music.artist  = tr.children().first().next().text();
-					var music_disco  = tr.children().first().next().next().text();
-					music.music = music_disco.match(/[^(]*/)[0].trim();
-					found = music_disco.match(/[(].*[)]/);
-					if (found) {
-						music.disco =  found[0];
-					}
-					music.id = hash(music);
+			let music_list = []
 
-					music_list.push(music);
+			let year = date.substr(4,4);
+			let month = date.substr(2,2);
+			let day = date.substr(0,2);
+
+			let url_date = day+month+year;
+
+			console.log('requesting '+url_date);
+			let url = `http://www.clicrbs.com.br/especial/sc/itapemafmsc/65,434,15,2,5,2,${url_date},PlayList.html`;
+			request(url).then( html => {
+				var $ = cheerio.load(html);
+				$('table.grade tbody').each( (index, value) => {
+					let tbody = $(value);
+					$('tr', tbody).each ( (index, value) => {
+						let tr = $(value);
+						var music = {}
+						music.origin = 'Itapema'
+						music.year = year;
+						music.month = month;
+						music.day = day;
+						music.time = tr.children().first().text();
+						music.artist  = tr.children().first().next().text();
+						var music_disco  = tr.children().first().next().next().text();
+						music.music = music_disco.match(/[^(]*/)[0].trim();
+						found = music_disco.match(/[(].*[)]/);
+						if (found) {
+							music.disco =  found[0];
+						}
+						music.id = hash(music);
+
+						music_list.push(music);
+					});
 				});
-			});
-			resolve(music_list);
-		}).catch(console.log);
+				resolve(music_list);
+			}).catch(console.log);
+
+		});
+
 	})
+
+
+
 }
 
 var stmt = db.prepare("INSERT INTO ITAPEMA_MUSIC (id, origin, year, month, day, time, artist, music, disc) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
